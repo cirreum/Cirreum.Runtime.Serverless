a0e3b327f9e102ad29c88940d8fe80ef371e732b
Glen Banta <glen.banta@outlook.com>
2026-01-11 23:12:02 -0700

    added DomainContext Initializer

---
+++ src/Cirreum.Runtime.Serverless/SystemInitializers/InitializeDomainContext.cs
@@ -0,0 +1,13 @@
+﻿namespace Cirreum.Runtime.SystemInitializers;
+
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading.Tasks;
+
+internal class InitializeDomainContext : ISystemInitializer {
+	public ValueTask RunAsync(IServiceProvider serviceProvider) {
+		var initializer = serviceProvider.GetRequiredService<IDomainContextInitializer>();
+		initializer.Initialize();
+		return ValueTask.CompletedTask;
+	}
+}
